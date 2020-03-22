4f75d65e6722ea55e62cac8139b4fbaa7f11db3a
gentledot <gentledot.wp@gmail.com>
2020-03-23 02:53:56 +0900

    WebSocketDemo - js, css 위치 이동 및 htmlEscape()의 반환값 확인 추가

--- src/main/java/net/gentledot/websocket/controllers/GreetingController.java
+++ src/main/java/net/gentledot/websocket/controllers/GreetingController.java
@@ -2,6 +2,8 @@ package net.gentledot.websocket.controllers;
 
 import net.gentledot.websocket.models.messages.Greeting;
 import net.gentledot.websocket.models.messages.HelloMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -9,10 +11,15 @@ import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class GreetingController {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
         Thread.sleep(1000); // simulated delay
-        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+        String htmlStr = HtmlUtils.htmlEscape(message.getName());
+
+        log.debug("생성된 HTML 문자열 : {}", htmlStr);
+        return new Greeting("Hello, " + htmlStr + "!");
     }
 }
